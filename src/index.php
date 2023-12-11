dfc688c522dcf33bf563c9f114bd7bae9a1394b9
Jacob Nelson <jacob.nelson03@googlemail.com>
2023-12-11 13:53:10 +0000

    Add feature to change page every 30 seconds

--- src/index.php
+++ src/index.php
@@ -1,8 +1,33 @@
 <?php
 require_once 'Models/businessDataSet.php';
 
+$views = [
+    1 => 'Views/business.phtml',
+    2 => 'Views/commercialAndAccounts.phtml',
+    3 => 'Views/healthAndSafety.phtml',
+    4 => 'Views/hr.phtml',
+    5 => 'Views/itAndSystems.phtml'
+];
 
-$view = new stdClass();
-$businessDataSet = new BusinessDataSet();
-$view->businessDataSet = $businessDataSet->fetchBusinessFromMonth(12);
-require_once('Views/business.phtml');
+$currentViewKey = isset($_GET['view']) ? (int)$_GET['view'] : 1;
+
+if (!array_key_exists($currentViewKey, $views))
+{
+    $currentViewKey = 1;
+}
+
+include $views[$currentViewKey];
+?>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        setInterval(changeView, 3000); // Change view every 30 seconds
+    });
+
+    function changeView() {
+        var currentView = <?php echo $currentViewKey; ?>;
+        var nextView = (currentView % 5) + 1;
+        window.location.href = '?view=' + nextView;
+    }
+</script>
+
