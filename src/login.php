dc6bd94559aa704e972ef84c96b46c20a126f017
Bradley Bellairs <bwbellairs@outlook.com>
2023-12-12 23:53:14 +0000

    Add basic login testing

--- src/login.php
+++ src/login.php
@@ -1,2 +1,12 @@
 <?php
+
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if ($password == "testing") {
+        header('Location: adminpage.php');
+    }
+}
+
 require_once('Views/login.phtml');
