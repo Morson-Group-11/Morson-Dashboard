d3afc5fbc7a6be03f4bab73cf3e54acac3919512
Jacob Nelson <j.nelson10@edu.salford.ac.uk>
2023-12-14 23:26:41 +0000

    Increase delay

--- src/index.php
+++ src/index.php
@@ -62,7 +62,7 @@
                         setTimeout(() => {
                             console.log('Calling updateView function for:', data.viewName);
                             updateView(abortController.signal);
-                        }, 1000); // 100 ms delay
+                        }, 3000); // 100 ms delay
                     }
                 };
 
