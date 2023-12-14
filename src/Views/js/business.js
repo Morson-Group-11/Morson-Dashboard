795461e80fb835358d426070c41acb59b15b9569
Jacob Nelson <j.nelson10@edu.salford.ac.uk>
2023-12-14 22:25:59 +0000

    Alter business.js

--- src/Views/js/business.js
+++ src/Views/js/business.js
@@ -1,18 +1,39 @@
-function updateView() {
-    fetch('/fetchViewData.php')
-        .then(response => response.json())
-        .then(data => {
-            if (data.departmentDataSet && data.departmentDataSet.business_development) {
-                var businessDataArray = data.departmentDataSet.business_development;
-                var latestBusinessData = businessDataArray[businessDataArray.length - 1]; // Get the latest data entry
-
-                document.getElementById('liveBids').innerText = latestBusinessData['live_bids'] || 'No data available.';
-                document.getElementById('totalBids').innerText = '£' + (latestBusinessData['value_live_bids'] ? Number(latestBusinessData['value_live_bids']).toLocaleString() : 'No data available.');
-                document.getElementById('currentLeads').innerText = latestBusinessData['current_leads'] || 'No data available.';
-            }
-            setTimeout(updateView, 30000);
-        })
-        .catch(error => console.error('Error:', error));
+function updateBusinessData(businessData) {
+    document.getElementById('liveBids').innerText = businessData['live_bids'] || 'No data available.';
+    document.getElementById('totalBids').innerText = '£' + (businessData['value_live_bids'] || '0');
+    document.getElementById('currentLeads').innerText = businessData['current_leads'] || 'No data available.';
 }
 
-updateView(); // Initial call to start the rotation
+function updateView() {
+    console.log('Fetching business data...');
+    fetch('/fetchViewData.php')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Server response:', data);
+            if (data.departmentDataSet && data.departmentDataSet.business_development) {
+                var businessDataArray = data.departmentDataSet.business_development;
+                var selectedMonthData = businessDataArray.find(entry => entry.month === 12); // Adjust month as needed
+                if (selectedMonthData) {
+                    updateBusinessData(selectedMonthData);
+                } else {
+                    console.log('No data available for the selected month');
+                }
+            } else {
+                console.log('No business development data found');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching and updating business data:', error);
+        });
+}
+
+// Schedule the function to run every 30 seconds
+setInterval(updateView, 30000);
+
+// Initial call to start the data fetch and view update process
+updateView();
